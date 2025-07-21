3cfa703f3cefca0494456b6f07488623be3ff8ae
t.latush <t.latush@t-latush.local>
2025-07-21 10:11:40 +0200

    fqwf

--- src/screens/PopularPlaceCasd.js
+++ src/screens/PopularPlaceCasd.js
@@ -1,12 +1,141 @@
+// import React from 'react';
+// import {View, Text, StyleSheet, Image, TouchableOpacity, Alert, Linking, ScrollView} from 'react-native';
+// import Share from 'react-native-share';
+// import { useDispatch } from 'react-redux';
+// import {savePlace} from "../redux/slices/saveSlice";
+//
+//
+// export default function PopularPlaceCard({route}) {
+//     const {place} = route.params;
+//     const dispatch = useDispatch();
+//
+//     const handleShare = async () => {
+//         try {
+//             await Share.open({
+//                 title: place.title,
+//                 message: `${place.title}\n\n${place.description}\n\nLocation: ${place.coordinates.latitude}, ${place.coordinates.longitude}`,
+//             });
+//         } catch (error) {
+//             console.error('Share error:', error);
+//         }
+//     };
+//
+//     const handleOpenMap = () => {
+//         const url = `https://www.google.com/maps?q=${place.coordinates.latitude},${place.coordinates.longitude}`;
+//         Linking.openURL(url).catch(() => Alert.alert('Error', 'Cannot open map'));
+//     };
+//
+//     const handleSave = () => {
+//         // console.log(place);
+//         dispatch(savePlace(place));
+//         Alert.alert('Saved', 'Place has been saved!');
+//     };
+//
+//     return (
+//         <ScrollView style={styles.container}>
+//         <View style={styles.card}>
+//             <Text style={styles.heading}>Popular place</Text>
+//             <Image source={place.img} style={styles.image} />
+//             <Text style={styles.title}>{place.title}</Text>
+//             <Text style={styles.stars}>⭐️⭐️⭐️⭐️⭐️</Text>
+//             <Text style={styles.coords}>
+//                 {place.coordinates.latitude}, {place.coordinates.longitude}
+//             </Text>
+//             <Text style={styles.description}>{place.description}</Text>
+//             <View style={styles.buttons}>
+//                 <TouchableOpacity style={styles.btn} onPress={handleOpenMap}>
+//                     <Text>Open on map</Text>
+//                     <Image source={require('../assets/img/famicons_open-outline.png')}/>
+//                 </TouchableOpacity>
+//                 <TouchableOpacity style={styles.btn} onPress={handleSave}>
+//                     <Text>Save</Text>
+//                     <Image source={require('../assets/img/qwffff.png')}/>
+//                 </TouchableOpacity>
+//                 <TouchableOpacity style={styles.btn} onPress={handleShare}>
+//                     <Text>Share</Text>
+//                     <Image source={require('../assets/img/hugeicons_share-01.png')}/>
+//                 </TouchableOpacity>
+//             </View>
+//         </View>
+//         </ScrollView>
+//     );
+// }
+//
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: '#000',
+//         padding: 16,
+//     },
+//     card: {
+//         backgroundColor: '#191919',
+//         borderRadius: 12,
+//         padding: 16,
+//         // marginTop: 106,
+//         margin: 16,
+//     },
+//     heading: {
+//         color: '#fff',
+//         fontSize: 18,
+//         fontWeight: 'bold',
+//         marginBottom: 8,
+//         alignSelf: 'center',
+//     },
+//     image: {
+//         height: 180,
+//         width: '100%',
+//         borderRadius: 8,
+//         marginBottom: 12,
+//     },
+//     title: {
+//         fontSize: 20,
+//         color: '#fff',
+//         fontWeight: 'bold',
+//     },
+//     stars: {
+//         fontSize: 16,
+//         color: '#FFD700',
+//     },
+//     coords: {
+//         fontSize: 14,
+//         color: '#bbb',
+//         marginVertical: 4,
+//     },
+//     description: {
+//         color: '#ccc',
+//         fontSize: 14,
+//         marginBottom: 10,
+//     },
+//     buttons: {
+//         flexDirection: 'row',
+//         justifyContent: 'space-between',
+//     },
+//     btn: {
+//         backgroundColor: '#fff',
+//         padding: 10,
+//         flexDirection: 'row',alignItems: 'center',gap: 4,
+//         borderRadius: 8,
+//     },
+// });
+
 import React from 'react';
-import {View, Text, StyleSheet, Image, TouchableOpacity, Alert, Linking, ScrollView} from 'react-native';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Image,
+    TouchableOpacity,
+    Alert,
+    Linking,
+    ScrollView
+} from 'react-native';
 import Share from 'react-native-share';
 import { useDispatch } from 'react-redux';
-import {savePlace} from "../redux/slices/saveSlice";
+import { savePlace } from '../redux/slices/saveSlice';
+import MapView, { Marker } from 'react-native-maps';
 
-
-export default function PopularPlaceCard({route}) {
-    const {place} = route.params;
+export default function PopularPlaceCard({ route }) {
+    const { place } = route.params;
     const dispatch = useDispatch();
 
     const handleShare = async () => {
@@ -20,43 +149,54 @@ export default function PopularPlaceCard({route}) {
         }
     };
 
-    const handleOpenMap = () => {
-        const url = `https://www.google.com/maps?q=${place.coordinates.latitude},${place.coordinates.longitude}`;
-        Linking.openURL(url).catch(() => Alert.alert('Error', 'Cannot open map'));
-    };
-
     const handleSave = () => {
-        // console.log(place);
         dispatch(savePlace(place));
         Alert.alert('Saved', 'Place has been saved!');
     };
 
     return (
         <ScrollView style={styles.container}>
-        <View style={styles.card}>
-            <Text style={styles.heading}>Popular place</Text>
-            <Image source={place.img} style={styles.image} />
-            <Text style={styles.title}>{place.title}</Text>
-            <Text style={styles.stars}>⭐️⭐️⭐️⭐️⭐️</Text>
-            <Text style={styles.coords}>
-                {place.coordinates.latitude}, {place.coordinates.longitude}
-            </Text>
-            <Text style={styles.description}>{place.description}</Text>
-            <View style={styles.buttons}>
-                <TouchableOpacity style={styles.btn} onPress={handleOpenMap}>
-                    <Text>Open on map</Text>
-                    <Image source={require('../assets/img/famicons_open-outline.png')}/>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.btn} onPress={handleSave}>
-                    <Text>Save</Text>
-                    <Image source={require('../assets/img/qwffff.png')}/>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.btn} onPress={handleShare}>
-                    <Text>Share</Text>
-                    <Image source={require('../assets/img/hugeicons_share-01.png')}/>
-                </TouchableOpacity>
+            <View style={styles.card}>
+                <Text style={styles.heading}>Popular place</Text>
+                <Image source={place.img} style={styles.image} />
+                <Text style={styles.title}>{place.title}</Text>
+                <Text style={styles.stars}>⭐️⭐️⭐️⭐️⭐️</Text>
+                <Text style={styles.coords}>
+                    {place.coordinates.latitude}, {place.coordinates.longitude}
+                </Text>
+                <Text style={styles.description}>{place.description}</Text>
+
+                {/* ВСТРОЕННАЯ КАРТА */}
+                <MapView
+                    style={styles.map}
+                    initialRegion={{
+                        latitude: place.coordinates.latitude,
+                        longitude: place.coordinates.longitude,
+                        latitudeDelta: 0.01,
+                        longitudeDelta: 0.01,
+                    }}
+                >
+                    <Marker
+                        coordinate={{
+                            latitude: place.coordinates.latitude,
+                            longitude: place.coordinates.longitude,
+                        }}
+                        title={place.title}
+                        description={place.description}
+                    />
+                </MapView>
+
+                <View style={styles.buttons}>
+                    <TouchableOpacity style={styles.btn} onPress={handleSave}>
+                        <Text>Save</Text>
+                        <Image source={require('../assets/img/qwffff.png')} />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.btn} onPress={handleShare}>
+                        <Text>Share</Text>
+                        <Image source={require('../assets/img/hugeicons_share-01.png')} />
+                    </TouchableOpacity>
+                </View>
             </View>
-        </View>
         </ScrollView>
     );
 }
@@ -71,7 +211,6 @@ const styles = StyleSheet.create({
         backgroundColor: '#191919',
         borderRadius: 12,
         padding: 16,
-        // marginTop: 106,
         margin: 16,
     },
     heading: {
@@ -106,6 +245,11 @@ const styles = StyleSheet.create({
         fontSize: 14,
         marginBottom: 10,
     },
+    map: {
+        height: 200,
+        borderRadius: 10,
+        marginVertical: 12,
+    },
     buttons: {
         flexDirection: 'row',
         justifyContent: 'space-between',
@@ -113,7 +257,9 @@ const styles = StyleSheet.create({
     btn: {
         backgroundColor: '#fff',
         padding: 10,
-        flexDirection: 'row',alignItems: 'center',gap: 4,
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 4,
         borderRadius: 8,
     },
 });
